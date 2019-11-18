4cc8e7882075ebaae3ee7cc032ddcd54699d9e76
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-11-18 15:20:20 -0800

    mine working with 3 difficulty

--- mine.py
+++ mine.py
@@ -1,72 +1,62 @@
 import hashlib
 import requests
-
-import sys
-
 import random
 
 
 def proof_of_work(last_proof):
 
     print("Searching for next proof")
-    proof = random.randint(1000000, 999999999)
+    proof = random.randint(10000000, 999999999)
 
-    last_proof_encoded = f"{last_proof}".encode()
+    while not valid_proof(last_proof, proof):
+        proof = random.randint(10000000, 999999999)
 
-    last_hash = hashlib.sha256(last_proof_encoded).hexdigest()
+    print("PROOF FOUND", proof)
 
-    counter = 0
-
-    while not valid_proof(last_hash, proof):
-        proof = random.randint(1000000, 999999999)
-
-        counter += 1
-
-        if counter == 1000000:
-            print(f"at {counter}")
-            if requests.get(url=node_last_proof).json()["proof"] != last_proof:
-                print(f"someone else found a proof")
-                counter = 0
-                return None
-            counter = 0
     return proof
 
 
-def valid_proof(last_hash, proof):
-    new_encoded = f"{proof}".encode()
+def valid_proof(last_proof, proof):
+    last_proof_encoded = f"{last_proof}".encode()
+
+    new_encoded = f"{last_proof_encoded}{proof}".encode()
 
     new_hash = hashlib.sha256(new_encoded).hexdigest()
 
-    return last_hash[-6:] == new_hash[:6]
+    print("new_hash", new_hash)
+
+    return new_hash[:3] == "000"
 
 
 if __name__ == '__main__':
 
-    node_last_proof = "https://lambda-treasure-hunt.herokuapp.com/api/bc/last_proof"
-    node_mine = "https://lambda-treasure-hunt.herokuapp.com/api/bc/mine/"
+    proof_of_work(123456)
 
-    coins_mined = 0
+    # node_last_proof = "https://lambda-treasure-hunt.herokuapp.com/api/bc/last_proof"
+    # node_mine = "https://lambda-treasure-hunt.herokuapp.com/api/bc/mine/"
 
-    f = open("token.txt", "r")
-    id = f.read()
-    print("Token is", id)
-    f.close()
+    # coins_mined = 0
 
-    while True:
-        # Get the last proof from the server
-        r = requests.get(url=node_last_proof)
-        data = r.json()
-        new_proof = proof_of_work(data.get('proof'))
+    # f = open("token.txt", "r")
+    # id = f.read()
+    # print("Token is", id)
+    # f.close()
 
-        if new_proof is not None:
-            post_data = {"proof": new_proof,
-                         "id": id}
+    # while True:
+    #     # Get the last proof from the server
+    #     r = requests.get(url=node_last_proof)
+    #     data = r.json()
+    #     new_proof = proof_of_work(data.get('proof'))
 
-            r = requests.post(url=node_mine, json=post_data)
-            data = r.json()
-            print(data)
-            if data.get('message') == 'New Block Forged':
-                coins_mined += 1
-                print("Total coins mined: " + str(coins_mined))
-            else:
-                print(data.get('message'))
+    #     if new_proof is not None:
+    #         post_data = {"proof": new_proof,
+    #                      "id": id}
+
+    #         r = requests.post(url=node_mine, json=post_data)
+    #         data = r.json()
+    #         print(data)
+    #         if data.get('message') == 'New Block Forged':
+    #             coins_mined += 1
+    #             print("Total coins mined: " + str(coins_mined))
+    #         else:
+    #             print(data.get('message'))
