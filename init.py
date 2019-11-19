502c4d3f14f8d7ed3b12d1f8f7c355faea100153
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-11-19 10:39:55 -0800

    move request attempt

--- init.py
+++ init.py
@@ -6,27 +6,38 @@ import json
 
 import time
 
+graph = {
+    0: {"n": "?", "s": "?", "e": "?", "w": "?"}
+}
+
 if __name__ == '__main__':
-    # What is the server address? IE `python3 miner.py https://server.com/api/`
-    if len(sys.argv) > 1:
-        node = sys.argv[1]
-    else:
-        node = "https://lambda-treasure-hunt.herokuapp.com/api/adv/init/"
+    init_url = "https://lambda-treasure-hunt.herokuapp.com/api/adv/init/"
 
-    # Load ID
-    # f = open("christian.txt", "r")
-    # id = f.readline()
-    # print("ID is", id)
-    # f.close()
+    init = requests.get(url=init_url, headers={
+        'Authorization': 'Token e9697a598203554fa1e1e59f590f4b2e87a662be'})
 
-    # Run forever until interrupted
-    r = requests.get(url=node, headers={
-                     'Authorization': 'Token e9697a598203554fa1e1e59f590f4b2e87a662be'})
-    # Handle non-json response
-    try:
-        data = r.json()
-        print(data)
-    except ValueError:
-        print("Error:  Non-json response")
-        print("Response returned:")
-        print(r)
+    init_data = init.json()
+
+    # print(init_data)
+
+    move_url = "https://lambda-treasure-hunt.herokuapp.com/api/adv/move/"
+
+    send = {
+        "direction": "n"
+    }
+
+    move = requests.post(url=move_url, data=json.dumps(send), headers={
+        'Authorization': 'Token e9697a598203554fa1e1e59f590f4b2e87a662be',
+        'content-type': 'application/json',
+    })
+
+    move_data = move.json()
+
+    print(move_data)
+
+    # graph[move_data["room_id"]] = {}
+
+    # for exit in move_data["exits"]:
+    #     graph[move_data["room_id"]][exit] = "?"
+
+    # print(graph)
