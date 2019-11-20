9149e35fcd3e2e48656dbc2a60cdfeff622284f6
chris.j.carter <chris.j.carter91@gmail.com>
2019-11-20 15:59:18 -0700

    Mined Coin - Just insert token

--- mine_coin.js
+++ mine_coin.js
@@ -1,61 +1,58 @@
 // Run `node mine_coin.js` once player is in mine room
-
 const axios = require('axios');
 const shajs = require('sha.js');
-
+let last_proof = null;
 const mine_coin = async () => {
-  let last_proof = null;
-
   try {
     const response = await axios.get(
       'https://lambda-treasure-hunt.herokuapp.com/api/bc/last_proof/',
       {
         headers: {
-          Authorization: 'Token e9697a598203554fa1e1e59f590f4b2e87a662be'
+          Authorization: 'Token 308e0075ee15546fdcf031167af4c40346ad11fa'
         }
       }
     );
-
+    console.log('get last proof', response.data);
     last_proof = response.data.proof;
   } catch (error) {
     console.log(error);
   }
-
+  console.log('END', last_proof);
   const hash = (attempt) => {
     return shajs('sha256')
       .update(attempt)
       .digest('hex');
   };
-
   const valid_proof = (last_proof, proof) => {
     const guess_hash = hash(`${last_proof}${proof}`);
-
-    let leadingZeros = '00000000';
-
+    // current dificulty 6 - 6 zeros
+    let leadingZeros = '000000';
+    // console.log(guess_hash);
     return guess_hash.startsWith(leadingZeros);
   };
-
   let proof = Math.floor(Math.random() * 1000000000);
-
   while (!valid_proof(last_proof, proof)) {
     proof = Math.floor(Math.random() * 1000000000);
   }
-
-  try {
-    const response = await axios.post(
-      'https://lambda-treasure-hunt.herokuapp.com/api/bc/mine',
-      {
-        proof: proof,
-        headers: {
-          Authorization: 'Token e9697a598203554fa1e1e59f590f4b2e87a662be'
+  console.log(('PROOF FOUND', proof));
+  const SEND_PROOF = async () => {
+    try {
+      const response = await axios.post(
+        'https://lambda-treasure-hunt.herokuapp.com/api/bc/mine',
+        {
+          proof: proof
+        },
+        {
+          headers: {
+            Authorization: 'Token 308e0075ee15546fdcf031167af4c40346ad11fa'
+          }
         }
-      }
-    );
-
-    console.log('Proof found', response);
-  } catch (error) {
-    console.log('error finding proof', error);
-  }
+      );
+      console.log('Proof found');
+    } catch (error) {
+      console.log('error finding proof');
+    }
+  };
+  SEND_PROOF();
 };
-
-module.exports = mine_coin;
+mine_coin();
