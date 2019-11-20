b5df4904750c96d3f1ac900cb894f611cfa8f1cf
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-11-20 14:09:07 -0800

    get last proof

--- mine_coin.js
+++ mine_coin.js
@@ -1,65 +1,67 @@
 // Run `node mine_coin.js` once player is in mine room
 
-const axios = require("axios");
-const shajs = require("sha.js");
+const axios = require('axios');
+const shajs = require('sha.js');
 
 let last_proof = null;
 
-const getLastProof = async () => {
+const mine_coin = async () => {
   try {
     const response = await axios.get(
-      "https://lambda-treasure-hunt.herokuapp.com/api/bc/last_proof/",
+      'https://lambda-treasure-hunt.herokuapp.com/api/bc/last_proof/',
       {
         headers: {
-          Authorization: "Token e9697a598203554fa1e1e59f590f4b2e87a662be",
-        },
-      },
+          Authorization: 'Token e9697a598203554fa1e1e59f590f4b2e87a662be'
+        }
+      }
     );
-
+    console.log('get last proof', response.data);
     last_proof = response.data.proof;
   } catch (error) {
     console.log(error);
   }
+
+  console.log('END', last_proof);
+
+  // const hash = (attempt) => {
+  //   return shajs('sha256')
+  //     .update(attempt)
+  //     .digest('hex');
+  // };
+
+  // const valid_proof = (last_proof, proof) => {
+  //   const guess_hash = hash(`${last_proof}${proof}`);
+
+  //   let leadingZeros = '00000000';
+
+  //   return guess_hash.startsWith(leadingZeros);
+  // };
+
+  // let proof = Math.floor(Math.random() * 1000000000);
+
+  // while (!valid_proof(last_proof, proof)) {
+  //   proof = Math.floor(Math.random() * 1000000000);
+  // }
+
+  // const mineCoin = async () => {
+  //   try {
+  //     const response = await axios.post(
+  //       'https://lambda-treasure-hunt.herokuapp.com/api/bc/mine',
+  //       {
+  //         proof: proof,
+  //         headers: {
+  //           Authorization: 'Token e9697a598203554fa1e1e59f590f4b2e87a662be'
+  //         }
+  //       }
+  //     );
+
+  //     console.log('Proof found', response);
+  //   } catch (error) {
+  //     console.log('error finding proof', error);
+  //   }
+  // };
+
+  // mineCoin();
 };
 
-getLastProof();
-
-const hash = (attempt) => {
-  return shajs("sha256")
-    .update(attempt)
-    .digest("hex");
-};
-
-const valid_proof = (last_proof, proof) => {
-  const guess_hash = hash(`${last_proof}${proof}`);
-
-  let leadingZeros = "00000000";
-
-  return guess_hash.startsWith(leadingZeros);
-};
-
-let proof = Math.floor(Math.random() * 1000000000);
-
-while (!valid_proof(last_proof, proof)) {
-  proof = Math.floor(Math.random() * 1000000000);
-}
-
-const mineCoin = async () => {
-  try {
-    const response = await axios.post(
-      "https://lambda-treasure-hunt.herokuapp.com/api/bc/mine",
-      {
-        proof: proof,
-        headers: {
-          Authorization: "Token e9697a598203554fa1e1e59f590f4b2e87a662be",
-        },
-      },
-    );
-
-    console.log("Proof found", response);
-  } catch (error) {
-    console.log("error finding proof", error);
-  }
-};
-
-mineCoin();
+mine_coin();
