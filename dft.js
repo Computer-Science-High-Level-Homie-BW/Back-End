ac8b904564d3ae80aaa3ccca6bf3b2ed64b441f5
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-11-23 15:44:42 -0800

    dft

--- dft.js
+++ dft.js
@@ -23,128 +23,128 @@ g = new Graph();
 
 g.add_vertex(0);
 
-// const dft_until_dead_end = async (move_direction) => {
-//   setTimeout(() => {
-//     move(move_direction);
-//   }, 16000);
+const dft_until_dead_end = async (move_direction) => {
+  setTimeout(() => {
+    move(move_direction);
+  }, 16000);
 
-//   console.log("move made");
-//   for (let direction in g.vertices[current_room]) {
-//     if (g.vertices[current_room][direction] === "?") {
-//       dft_until_dead_end(direction);
-//       return;
-//     }
-//   }
-// };
+  console.log("move made");
+  for (let direction in g.vertices[current_room]) {
+    if (g.vertices[current_room][direction] === "?") {
+      dft_until_dead_end(direction);
+      return;
+    }
+  }
+};
 
-// const move = async (direction) => {
-//   let previous_room = current_room;
+const move = async (direction) => {
+  let previous_room = current_room;
 
-//   try {
-//     const response = await axios.post(
-//       "https://lambda-treasure-hunt.herokuapp.com/api/adv/move/",
-//       {
-//         direction: direction,
-//       },
-//       {
-//         headers: {
-//           Authorization: "Token e9697a598203554fa1e1e59f590f4b2e87a662be",
-//         },
-//       },
-//     );
+  try {
+    const response = await axios.post(
+      "https://lambda-treasure-hunt.herokuapp.com/api/adv/move/",
+      {
+        direction: direction,
+      },
+      {
+        headers: {
+          Authorization: "Token e9697a598203554fa1e1e59f590f4b2e87a662be",
+        },
+      },
+    );
 
-//     current_room = response.data.room_id;
-//     current_room_exits = response.data.exits;
+    current_room = response.data.room_id;
+    current_room_exits = response.data.exits;
 
-//     g.add_edges(previous_room, direction, current_room);
-//     g.add_vertex(current_room);
+    g.add_edges(previous_room, direction, current_room);
+    g.add_vertex(current_room);
 
-//     opposite_direction = null;
+    opposite_direction = null;
 
-//     if (direction === "w") {
-//       opposite_direction = "e";
-//     } else if (direction === "e") {
-//       opposite_direction = "w";
-//     } else if (direction === "s") {
-//       opposite_direction = "n";
-//     } else {
-//       opposite_direction = "s";
-//     }
+    if (direction === "w") {
+      opposite_direction = "e";
+    } else if (direction === "e") {
+      opposite_direction = "w";
+    } else if (direction === "s") {
+      opposite_direction = "n";
+    } else {
+      opposite_direction = "s";
+    }
 
-//     g.add_edges(current_room, opposite_direction, previous_room);
+    g.add_edges(current_room, opposite_direction, previous_room);
 
-//     for (let direction of current_room_exits) {
-//       if (!(direction in g.vertices[current_room])) {
-//         g.add_edges(current_room, direction, "?");
-//       }
-//     }
-//   } catch (error) {
-//     console.log("ERROR MOVING");
-//   }
-// };
+    for (let direction of current_room_exits) {
+      if (!(direction in g.vertices[current_room])) {
+        g.add_edges(current_room, direction, "?");
+      }
+    }
+  } catch (error) {
+    console.log("ERROR MOVING");
+  }
+};
 
-// const bft_shortest_path_to_unexplored_room = (starting_room) => {
-//   queue = [];
-//   visited = new Set();
+const bft_shortest_path_to_unexplored_room = (starting_room) => {
+  queue = [];
+  visited = new Set();
 
-//   queue.push([current_room]);
+  queue.push([current_room]);
 
-//   while (queue.length > 0) {
-//     let get_path = queue.shift();
+  while (queue.length > 0) {
+    let get_path = queue.shift();
 
-//     let last_id_from_path = get_path[get_path.length - 1];
+    let last_id_from_path = get_path[get_path.length - 1];
 
-//     if (!(last_id_from_path in visited)) {
-//       for (key in g.vertices[last_id_from_path]) {
-//         if (g.vertices[last_id_from_path][key] === "?") {
-//           return get_path;
-//         }
-//       }
+    if (!(last_id_from_path in visited)) {
+      for (key in g.vertices[last_id_from_path]) {
+        if (g.vertices[last_id_from_path][key] === "?") {
+          return get_path;
+        }
+      }
 
-//       visited.add(last_id_from_path);
+      visited.add(last_id_from_path);
 
-//       for (let direction in g.vertices[last_id_from_path]) {
-//         queue.push([...get_path, g.vertices[last_id_from_path][direction]]);
-//       }
-//     }
-//   }
+      for (let direction in g.vertices[last_id_from_path]) {
+        queue.push([...get_path, g.vertices[last_id_from_path][direction]]);
+      }
+    }
+  }
 
-//   return null;
-// };
+  return null;
+};
 
-// const move_reverse_back = async (direction) => {
-//   setTimeout(async () => {
-//     try {
-//       const response = await axios.post(
-//         "https://lambda-treasure-hunt.herokuapp.com/api/adv/move/",
-//         {
-//           direction: direction,
-//         },
-//         {
-//           headers: {
-//             Authorization: "Token e9697a598203554fa1e1e59f590f4b2e87a662be",
-//           },
-//         },
-//       );
+const move_reverse_back = async (direction) => {
+  setTimeout(async () => {
+    try {
+      const response = await axios.post(
+        "https://lambda-treasure-hunt.herokuapp.com/api/adv/move/",
+        {
+          direction: direction,
+        },
+        {
+          headers: {
+            Authorization: "Token e9697a598203554fa1e1e59f590f4b2e87a662be",
+          },
+        },
+      );
 
-//       current_room = response.data.room_id;
-//     } catch (error) {
-//       console.log("ERROR move Reverse BACK");
-//     }
-//   }, 16000);
-// };
+      current_room = response.data.room_id;
+    } catch (error) {
+      console.log("ERROR move Reverse BACK");
+    }
+  }, 16000);
+};
 
-// const move_back = (shortest_path_array) => {
-//   while (shortest_path_array.length > 1) {
-//     get_id = shortest_path_array.shift();
+const move_back = (shortest_path_array) => {
+  while (shortest_path_array.length > 1) {
+    get_id = shortest_path_array.shift();
 
-//     for (let direction in g.vertices[get_id]) {
-//       if (g.vertices[get_id][direction] === shortest_path_array[0]) {
-//         move_reverse_back(direction);
-//       }
-//     }
-//   }
-// };
+    for (let direction in g.vertices[get_id]) {
+      if (g.vertices[get_id][direction] === shortest_path_array[0]) {
+        move_reverse_back(direction);
+      }
+    }
+  }
+};
 
 const startDFT = async () => {
   try {
@@ -184,25 +184,25 @@ const startDFT = async () => {
     console.log("CURRENT ROOM", current_room);
     dft_until_dead_end(get_unexplored_direction);
 
-  //   let shorest_path_to_unexplored_room = bft_shortest_path_to_unexplored_room(
-  //     current_room,
-  //   );
+    let shorest_path_to_unexplored_room = bft_shortest_path_to_unexplored_room(
+      current_room,
+    );
 
-  //   if (shorest_path_to_unexplored_room === null) {
-  //     return;
-  //   }
+    if (shorest_path_to_unexplored_room === null) {
+      return;
+    }
 
-  //   move_back(shorest_path_to_unexplored_room);
+    move_back(shorest_path_to_unexplored_room);
 
-  //   print("CURRENT ROOM AFTER", current_room);
+    print("CURRENT ROOM AFTER", current_room);
 
-  //   queue = [];
+    queue = [];
 
-  //   for (let direction in g.vertices[current_room]) {
-  //     if (g.vertices[current_room][direction] === "?") {
-  //       queue.push(direction);
-  //     }
-  //   }
+    for (let direction in g.vertices[current_room]) {
+      if (g.vertices[current_room][direction] === "?") {
+        queue.push(direction);
+      }
+    }
   }
 };
 
